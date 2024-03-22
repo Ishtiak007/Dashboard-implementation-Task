25f0f6d0f49f24aa0b44a139b0f083b962c4b54d
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-03-22 23:19:29 +0600

    Navbar added to the homepage

--- src/Layout/Mainlayout.jsx
+++ src/Layout/Mainlayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Page/Home/Navbar";
 
 
 const Mainlayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
